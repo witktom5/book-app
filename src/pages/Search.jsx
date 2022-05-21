1d088c3fff2dc14d1579ccdd8e40af985f523000
witktom5 <witktom5@gmail.com>
2022-05-21 17:57:45 +0200

    add some filters

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,8 +8,17 @@ import { toast } from 'react-toastify';
 function Search() {
   const [isLoading, setIsLoading] = useState(false);
   const [search, setSearch] = useState('');
+  const [languages, setLanguages] = useState('');
+  const [topic, setTopic] = useState('');
+  const [copyright, setCopyright] = useState({
+    copyrighted: false,
+    notCopyrighted: false,
+    unknown: false,
+  });
+  const [authorsBirthMax, setAuthorsBirthMax] = useState(null);
+  const [authorsDeathMax, setAuthorsDeathMax] = useState(null);
 
-  const API_URL = 'https://gutendex.com/books/?';
+  const API_URL = 'https://gutendex.com/books/?search=';
 
   const navigate = useNavigate();
 
@@ -19,10 +28,29 @@ function Search() {
     e.preventDefault();
     if (search.length < 3)
       return toast.info('You must enter at least 3 characters');
+
+    let query = search;
+
+    //  add copyright info to query
+    if (
+      copyright.copyrighted ||
+      copyright.notCopyrighted ||
+      copyright.unknown
+    ) {
+      query += '&copyright=';
+      if (copyright.copyrighted) query += 'true,';
+      if (copyright.notCopyrighted) query += 'false,';
+      if (copyright.unknown) query += 'null';
+    }
+
+    //  add topic to query
+
+    if (topic.length > 1) query += '&topic=' + topic;
+
     try {
       setIsLoading(true);
-      console.log(isLoading);
-      const res = await axios.get(`${API_URL}`);
+      console.log(query);
+      const res = await axios.get(API_URL + query);
       console.log(res.data);
     } catch (error) {
       console.log(error);
@@ -57,6 +85,94 @@ function Search() {
               >
                 <FaSearch className='w-5 h-5' />
               </button>
+            </div>
+            <div className='flex flex-wrap w-full md:w-4/5 lg:w-3/5 mt-4 mx-auto'>
+              <article className='collapse collapse-arrow mx-auto mt-2'>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>
+                  Languages
+                </div>
+                <div className='collapse-content'>
+                  <div className='form-control'>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>English</span>
+                      <input type='checkbox' class='checkbox' />
+                    </label>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>French</span>
+                      <input type='checkbox' class='checkbox' />
+                    </label>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>Polish</span>
+                      <input type='checkbox' class='checkbox' />
+                    </label>
+                  </div>
+                </div>
+              </article>
+              <article className='collapse collapse-arrow mx-auto mt-2'>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>Topic</div>
+                <div className='collapse-content'>
+                  <input
+                    type='text'
+                    placeholder='Enter topic...'
+                    className='input input-bordered w-full max-w-lg'
+                    value={topic}
+                    onChange={(e) => {
+                      setTopic(e.target.value);
+                    }}
+                  />
+                </div>
+              </article>
+              <article className='collapse collapse-arrow mx-auto mt-2'>
+                <input type='checkbox' />
+                <div className='collapse-title text-xl font-medium'>
+                  Copyright
+                </div>
+                <div className='collapse-content'>
+                  <div className='form-control w-40'>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>Copyrighted</span>
+                      <input
+                        type='checkbox'
+                        class='checkbox'
+                        onChange={() =>
+                          setCopyright({
+                            ...copyright,
+                            copyrighted: !copyright.copyrighted,
+                          })
+                        }
+                      />
+                    </label>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>Not copyrighted</span>
+                      <input
+                        type='checkbox'
+                        class='checkbox'
+                        onChange={() =>
+                          setCopyright({
+                            ...copyright,
+                            notCopyrighted: !copyright.notCopyrighted,
+                          })
+                        }
+                      />
+                    </label>
+                    <label className='label cursor-pointer'>
+                      <span className='label-text'>Unknown</span>
+                      <input
+                        type='checkbox'
+                        class='checkbox'
+                        onChange={() =>
+                          setCopyright({
+                            ...copyright,
+                            unknown: !copyright.unknown,
+                          })
+                        }
+                      />
+                    </label>
+                  </div>
+                </div>
+              </article>
             </div>
           </div>
         </form>
